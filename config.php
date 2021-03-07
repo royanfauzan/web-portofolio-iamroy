1b43143206d4280765ef38d2be2494b65133b1c2
royanfauzan <royanfauzan7@gmail.com>
2021-03-07 18:37:00 +0800

    ilangin config server

--- config.php
+++ config.php
@@ -5,10 +5,7 @@ $user = "root";
 $password = "";
 $nama_database = "webroyski";
 
-// $server = "sql305.byethost17.com";
-// $user = "b17_26804144";
-// $password = "Stupid125";
-// $nama_database = "b17_26804144_webroyski";
+
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
